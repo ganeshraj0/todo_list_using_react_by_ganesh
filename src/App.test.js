813d09ed845f2a8c2ff1ada9252b9bf61a093525
ganeshraj0 <ganesh636164@gmail.com>
2024-08-23 00:43:27 -0700

    addtestp

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders todo list link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/todo list/i);
   expect(linkElement).toBeInTheDocument();
 });
